7e1919147ac865bfba70543b8fa14e5ba4eabfe6
heidijane <heidi@heidisprouse.com>
2020-03-16 14:54:47 -0500

    Journal entries now show at the bottom

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -2,14 +2,28 @@
  *  Purpose: To render a single journal entry as an
  *           HTML representation of the data
  */
+
+//get corresponding emoji for journal entry mood
+// const getEmoji = (mood) => {
+//     const emoji = 😃;
+//     switch (mood) {
+//         case 'excited':
+//             emoji = 😊
+//             break;
+//         case 'excited';
+//             emoji = 😊
+//             break;
+
+//     }
+// }
+
 const JournalEntryComponent = (entry) => {
     return `
-        <section id="entry--${entry.id}" class="journalEntry">
-            <div class="journalEntry__concept">${entry.concept}</div>
-            <div class="journalEntry__mood">${entry.mood}</div>
+        <article id="entry--${entry.id}" class="journalEntry">
+            <div class="journalEntry__concept"><span class="journalEntry__mood">${entry.mood}</span${entry.concept}</div>
             <div class="journalEntry__entry">${entry.entryText}</div>
             <div class="journalEntry__date">${entry.date}</div>
-        </section>
+        </article>
     `
 }
 
